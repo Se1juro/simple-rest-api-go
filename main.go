83477b0b2777812e30c830ad022a036073016192
Daniel Morales <mralejo99@gmail.com>
2022-07-01 15:13:52 -0500

    change hello index

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("{\"message\":\"Hello John Doe\"}")
+	json.NewEncoder(w).Encode("{\"message\":\"Hello\"}")
 }
 
 func main() {
